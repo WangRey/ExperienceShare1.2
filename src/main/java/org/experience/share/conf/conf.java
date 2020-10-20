5d1935f6d6cdf6499f5889f359ccec912278cb35
WangRey <983604371@qq.com>
2020-10-20 12:03:28 +0800

    4.2_技术分享:SpringBoot-脱离xml_Qualifier注解和Autowired解决冲突

---
+++ src/main/java/org/experience/share/conf/conf.java
@@ -0,0 +1,25 @@
+package org.experience.share.conf;
+
+import org.experience.share.dao.ByOracle;
+import org.experience.share.dao.BySqlserver;
+import org.experience.share.dao.Database;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class conf {
+
+    @Bean
+    public Database bySqlserver(){
+
+        return new BySqlserver();
+
+    }
+    @Bean
+    public Database byOracle(){
+
+        return new ByOracle();
+
+    }
+
+}
--- src/main/java/org/experience/share/service/UserService.java
+++ src/main/java/org/experience/share/service/UserService.java
@@ -3,12 +3,14 @@ package org.experience.share.service;
 import org.experience.share.dao.ByOracle;
 import org.experience.share.dao.Database;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     @Autowired
+    @Qualifier(value = "bySqlserver")
     Database database;
 
     public void getUserInfo(){
